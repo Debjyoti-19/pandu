3f8284f2fc7dcc08a1cddac85d7ef7dd30671c90
Debjyoti-19 <debjyoti.xio@gmail.com>
2025-05-13 17:38:58 +0530

    basic signup login function added, basic routs added in  App.jsx

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,11 +1,3 @@
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
-
 function NavBar() {
   return (
     <div class="flex justify-center items-center flex-row bg-gray-200">
