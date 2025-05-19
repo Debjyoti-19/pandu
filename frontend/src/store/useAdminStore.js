0cce1e52a2a27babaf34c31784753251dfabf7f6
Debjyoti-19 <debjyoti.xio@gmail.com>
2025-05-19 17:36:30 +0530

    useAdminStore.js updated

--- frontend/src/store/useAdminStore.js
+++ frontend/src/store/useAdminStore.js
@@ -2,10 +2,6 @@ import { create } from 'zustand'
 import { axiosInstance } from '../lib/axios.js'
 
 export const useAuthStore = create((set) => ({
-    authUser: null,
-    isCheckingAuth: false,
-    isSigningup: false,
-    isLogining: false,
     isUploadingData: false,
 
     addProduct: async (data) => {
@@ -18,44 +14,6 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isUploadingData: false })
         }
-    },
-
-    checkAuth: async () => {
-        set({ isCheckingAuth: true })
-        try {
-            const res = await axiosInstance.get('/user/check')
-            set({ authUser: res.data })
-        } catch (error) {
-            console.log("Error in checkAuth : ", error)
-            set({ authUser: null })
-        } finally {
-            set({ isCheckingAuth: false })
-        }
-    },
-
-    signup: async (data) => {
-        set({ isSigningup: true })
-        try {
-            const res = await axiosInstance.post('/user/signup', data)
-            set({ authUser: res.data })
-        } catch (error) {
-            console.log("Error in signup : ", error)
-            set({ authUser: null })
-        } finally {
-            set({ isSigningup: false })
-        }
-    },
-
-    login: async(data) => {
-        set({ isLogining: true })
-        try {
-            const res = await axiosInstance.post('/user/login', data)
-            set({ authUser: res.data })
-        } catch (error) {
-            console.log("Error in login : ", error)
-            set({ authUser: null })
-        } finally {
-            set({ isLogining: false })
-        }
     }
+
 }))
