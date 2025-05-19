0f93545e5c55285ce823b26d16ad2f0328bc3df1
Debjyoti-19 <debjyoti.xio@gmail.com>
2025-05-19 17:34:59 +0530

    useAdminStore.js added

---
+++ frontend/src/store/useAdminStore.js
@@ -0,0 +1,61 @@
+import { create } from 'zustand'
+import { axiosInstance } from '../lib/axios.js'
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isCheckingAuth: false,
+    isSigningup: false,
+    isLogining: false,
+    isUploadingData: false,
+
+    addProduct: async (data) => {
+        set({ isUploadingData: true })
+        try {
+            const res = await axiosInstance.post('/product/add-item', data)
+            console.log(res.data)
+        } catch (error) {
+            console.log("Error in uploading product : ", error)
+        } finally {
+            set({ isUploadingData: false })
+        }
+    },
+
+    checkAuth: async () => {
+        set({ isCheckingAuth: true })
+        try {
+            const res = await axiosInstance.get('/user/check')
+            set({ authUser: res.data })
+        } catch (error) {
+            console.log("Error in checkAuth : ", error)
+            set({ authUser: null })
+        } finally {
+            set({ isCheckingAuth: false })
+        }
+    },
+
+    signup: async (data) => {
+        set({ isSigningup: true })
+        try {
+            const res = await axiosInstance.post('/user/signup', data)
+            set({ authUser: res.data })
+        } catch (error) {
+            console.log("Error in signup : ", error)
+            set({ authUser: null })
+        } finally {
+            set({ isSigningup: false })
+        }
+    },
+
+    login: async(data) => {
+        set({ isLogining: true })
+        try {
+            const res = await axiosInstance.post('/user/login', data)
+            set({ authUser: res.data })
+        } catch (error) {
+            console.log("Error in login : ", error)
+            set({ authUser: null })
+        } finally {
+            set({ isLogining: false })
+        }
+    }
+}))
