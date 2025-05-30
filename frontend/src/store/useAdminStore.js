71bb4248748b8091b244f3d2be763a9877de42bf
Debjyoti-19 <debjyoti.xio@gmail.com>
2025-05-30 22:14:35 +0530

    useAdminStore updated

--- frontend/src/store/useAdminStore.js
+++ frontend/src/store/useAdminStore.js
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
+import { useAuthStore } from "./useAuthStore.js";
 
 export const useAdminStore = create((set) => ({
     isUploadingData: false,
+    isGettingData: false,
 
     addProduct: async (data) => {
         set({ isUploadingData: true })
@@ -10,12 +12,22 @@ export const useAdminStore = create((set) => ({
             const res = await axiosInstance.post('/product/add-item', data, {
                 headers: { "Content-Type": "multipart/form-data" }
             })
-            console.log(res.data)
-            console.log("Product added")
         } catch (error) {
             console.log("Error in uploading product : ", error)
         } finally {
             set({ isUploadingData: false })
+        }
+    },
+    getProduct: async () => {
+        set({ isGettingData: true })
+        const { authUser } = useAuthStore()
+        try {
+            const res = await axiosInstance.post('/product/get-item', {
+                adminId: authUser._id
+            })
+            console.log(res)
+        } catch (error) {
+            
         }
     }
 }))
