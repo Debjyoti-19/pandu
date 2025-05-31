124d75c1dbf0b93d8f6fd9b883136f310ccc8ab3
Debjyoti-19 <debjyoti.xio@gmail.com>
2025-05-31 17:03:36 +0530

    logo changed

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import pandaImg from "../media/panda.jpeg";
+import pandaImg from "../media/panda.png";
 import { CircleUser } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
